dc7620d64f637bc36e40f5aad50315b677028b93
Jessie <jess.bell@me.com>
2017-12-01 11:48:12 -0800

    Begin to add functionality to display provider results

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,8 +1,51 @@
 import { Copy } from './../js/scripts.js';
 
 let displayData = function(results) {
+  console.log(results);
+
   for(let i=0; i<results.data.length; i++) {
-     $('.output2').append("<img src=" + results.data[i].profile.image_url + ">");
+    // $('.output').append(
+    //   "<div class='output" + [i]
+    //     + "'><img src="
+    //     + results.data[i].profile.image_url
+    //     + "> <h3>"
+    //     + results.data[i].profile.first_name
+    //     + " "
+    //     + results.data[i].profile.last_name
+    //     + "</h3><p>"
+    //     + results.data[i].profile.bio
+    //     + "</p><p>This practicioner is accepting new patients at this time: "
+    //     + results.data[i].practices[0].accepts_new_patients
+    //     + ".</p><p>"
+    //     + results.data[i].practices[0].visit_address.street
+    //     + "<br>"
+    //     + results.data[i].practices[0].visit_address.city
+    //     + ", "
+    //     + results.data[i].practices[0].visit_address.state
+    //     + " "
+    //     + results.data[i].practices[0].visit_address.zip
+    //     +"</p><p> Office Phone: "
+    //     + results.data[i].practices[0].phones[0].number
+    //     + "</p>"
+    //   + "</div>"
+    // );
+    let website = results.data[i].practices[0].website;
+    console.log(website);
+
+    if (typeof website != 'undefined') {
+      $('.output2').append(
+        "<div class='output-website"+ [i] + ">This provider has no website on file</div>"
+      );
+    } else {
+      console.log("yay!");
+      // $('.output2').append(
+      //   "<div class='output-website"
+      //   + [i]
+      //   + ">"
+      //   + results.data[i].practices[0].website
+      //   + "</div>"
+      // );
+    }
   }
 };
 
@@ -17,7 +60,7 @@ $(document).ready(function() {
   });
 
   $(".btn-error").click(function(event) {
-    $('.output2').empty();
+    $('.output').empty();
   });
 
 });
