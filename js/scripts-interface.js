4c680cf68258d5af89f69fd39e80ad71d9a13f1c
Jessie <jess.bell@me.com>
2017-12-01 12:35:11 -0800

    Fix problems with displaying website, add message to the user if there is no search results returned

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,51 +1,46 @@
 import { Copy } from './../js/scripts.js';
 
 let displayData = function(results) {
-  console.log(results);
+  if (results.data.length === 0) {
+    $('.output').append("<p class='no-return'>The search did not return any results. Please alter your search term and try again.</p>")
+  } else {
 
-  for(let i=0; i<results.data.length; i++) {
-    // $('.output').append(
-    //   "<div class='output" + [i]
-    //     + "'><img src="
-    //     + results.data[i].profile.image_url
-    //     + "> <h3>"
-    //     + results.data[i].profile.first_name
-    //     + " "
-    //     + results.data[i].profile.last_name
-    //     + "</h3><p>"
-    //     + results.data[i].profile.bio
-    //     + "</p><p>This practicioner is accepting new patients at this time: "
-    //     + results.data[i].practices[0].accepts_new_patients
-    //     + ".</p><p>"
-    //     + results.data[i].practices[0].visit_address.street
-    //     + "<br>"
-    //     + results.data[i].practices[0].visit_address.city
-    //     + ", "
-    //     + results.data[i].practices[0].visit_address.state
-    //     + " "
-    //     + results.data[i].practices[0].visit_address.zip
-    //     +"</p><p> Office Phone: "
-    //     + results.data[i].practices[0].phones[0].number
-    //     + "</p>"
-    //   + "</div>"
-    // );
-    let website = results.data[i].practices[0].website;
-    console.log(website);
+    for(let i=0; i<results.data.length; i++) {
 
-    if (typeof website != 'undefined') {
-      $('.output2').append(
-        "<div class='output-website"+ [i] + ">This provider has no website on file</div>"
+      let website = results.data[i].practices[0].website;
+
+      if (website === undefined) {
+        website = "The provider has not listed a website";
+      }
+
+      $('.output').append(
+        "<div class='output" + [i]
+          + "'><img src="
+          + results.data[i].profile.image_url
+          + "> <h3>"
+          + results.data[i].profile.first_name
+          + " "
+          + results.data[i].profile.last_name
+          + "</h3><p>"
+          + results.data[i].profile.bio
+          + "</p><p>This practicioner is accepting new patients at this time: "
+          + results.data[i].practices[0].accepts_new_patients
+          + ".</p><p>"
+          + results.data[i].practices[0].visit_address.street
+          + "<br>"
+          + results.data[i].practices[0].visit_address.city
+          + ", "
+          + results.data[i].practices[0].visit_address.state
+          + " "
+          + results.data[i].practices[0].visit_address.zip
+          +"</p><p> Office Phone: "
+          + results.data[i].practices[0].phones[0].number
+          + "</p><p>"
+          + website
+        + "</p></div>"
       );
-    } else {
-      console.log("yay!");
-      // $('.output2').append(
-      //   "<div class='output-website"
-      //   + [i]
-      //   + ">"
-      //   + results.data[i].practices[0].website
-      //   + "</div>"
-      // );
-    }
+
+      }
   }
 };
 
